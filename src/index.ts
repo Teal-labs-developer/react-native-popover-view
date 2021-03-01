623bddd13ae33f9ddef54a9dea0ce5479726ff66
Sahil-Choudhary-09 <79831895+Sahil-Choudhary-09@users.noreply.github.com>
2021-03-01 19:51:09 +0530

    Change naming conventions of files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as default } from './Popover.js'
-export { Placement as PopoverPlacement, Mode as PopoverMode } from './Constants';
-export { Rect as Rect, Size as Size } from './Utility.js'
+export { default as default } from './Popover.tsx'
+export { Placement as PopoverPlacement, Mode as PopoverMode } from './Constants.ts';
+export { Rect as Rect, Size as Size } from './Utility.ts'
